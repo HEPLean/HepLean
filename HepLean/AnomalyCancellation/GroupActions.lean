f42540ffb425949af9861dc91768975c5fc7b2e4
jstoobysmith <72603918+jstoobysmith@users.noreply.github.com>
2024-05-14 11:23:13 -0400

    doc: re-add doc

--- HepLean/AnomalyCancellation/GroupActions.lean
+++ HepLean/AnomalyCancellation/GroupActions.lean
@@ -53,6 +53,7 @@ def linSolMap {χ : ACCSystem} (G : ACCSystemGroupAction χ) (g : G.group) :
     apply ACCSystemLinear.LinSols.ext
     exact (G.rep g).map_smul' _ _
 
+/-- The representation acting on the vector space of solutions to the linear ACCs. -/
 @[simps!]
 def linSolRep {χ : ACCSystem} (G : ACCSystemGroupAction χ) :
     Representation ℚ G.group χ.LinSols where
