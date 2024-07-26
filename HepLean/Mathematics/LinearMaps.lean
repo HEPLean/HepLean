b26f9e66912edbc8cf3794b6047a647d2baa7b3a
jstoobysmith <72603918+jstoobysmith@users.noreply.github.com>
2024-07-26 16:32:54 -0400

    bump to v4.10.0-rc2

--- HepLean/Mathematics/LinearMaps.lean
+++ HepLean/Mathematics/LinearMaps.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joseph Tooby-Smith
 -/
 import Mathlib.Tactic.Polyrith
-import Mathlib.Algebra.Module.LinearMap.Basic
+import Mathlib.Algebra.Module.LinearMap.Defs
 import Mathlib.Data.Fintype.BigOperators
 /-!
 # Linear maps
