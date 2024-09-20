d713575b763675984f11d1720c02c16a9aa863a1
jstoobysmith <72603918+jstoobysmith@users.noreply.github.com>
2024-09-20 17:38:48 -0400

    docs: Add clusters to informal graph

--- HepLean/BeyondTheStandardModel/PatiSalam/Basic.lean
+++ HepLean/BeyondTheStandardModel/PatiSalam/Basic.lean
@@ -9,7 +9,7 @@ import HepLean.Meta.Informal
 
 # The Pati-Salam Model
 
-The Pati-Salam model is a grand unified theory that unifies the Standard Model gauge group into
+The Pati-Salam model is a petite unified theory that unifies the Standard Model gauge group into
 `SU(4) x SU(2) x SU(2)`.
 
 This file current contains informal-results about the Pati-Salam group.
@@ -27,12 +27,23 @@ informal_definition GaugeGroupI where
   math :≈ "The group `SU(4) x SU(2) x SU(2)`."
   physics :≈ "The gauge group of the Pati-Salam model (unquotiented by ℤ₂)."
 
-informal_definition embedSM where
-  physics :≈ "The embedding of the Standard Model gauge group into the Pati-Salam gauge group."
+informal_definition inclSM where
+  physics :≈ "The homomorphism of the Standard Model gauge group into the Pati-Salam gauge group."
   math :≈ "The group homomorphism `SU(3) x SU(2) x U(1) -> SU(4) x SU(2) x SU(2)`
     taking (h, g, α) to (blockdiag (α h, α ^ (-3)), g, diag(α ^ (3), α ^(-3)))."
   ref :≈ "Page 54 of https://math.ucr.edu/home/baez/guts.pdf"
   deps :≈ [``GaugeGroupI, ``StandardModel.GaugeGroupI]
+
+informal_lemma inclSM_ker where
+  math :≈ "The kernel of the map ``inclSM is equal to the subgroup
+    ``StandardModel.gaugeGroupℤ₃SubGroup."
+  ref :≈ "Footnote 10 of https://arxiv.org/pdf/2201.07245"
+  deps :≈ [``inclSM, ``StandardModel.gaugeGroupℤ₃SubGroup]
+
+informal_definition embedSMℤ₃ where
+  math :≈ "The group embedding from ``StandardModel.GaugeGroupℤ₃ to ``GaugeGroupI
+    induced by ``inclSM by quotienting by the kernal ``inclSM_ker."
+  deps :≈ [``inclSM, ``StandardModel.GaugeGroupℤ₃, ``GaugeGroupI, ``inclSM_ker]
 
 informal_definition gaugeGroupISpinEquiv where
   math :≈ "The equivalence between `GaugeGroupI` and `Spin(6) × Spin(4)`."
@@ -54,12 +65,12 @@ informal_definition GaugeGroupℤ₂ where
 informal_lemma sm_ℤ₆_factor_through_gaugeGroupℤ₂SubGroup where
   math :≈ "The group ``StandardModel.gaugeGroupℤ₆SubGroup under the homomorphism ``embedSM factors
     through the subgroup ``gaugeGroupℤ₂SubGroup."
-  deps :≈ [``embedSM, ``StandardModel.gaugeGroupℤ₆SubGroup, ``gaugeGroupℤ₂SubGroup]
+  deps :≈ [``inclSM, ``StandardModel.gaugeGroupℤ₆SubGroup, ``gaugeGroupℤ₂SubGroup]
 
 informal_definition embedSMℤ₆Toℤ₂ where
   math :≈ "The group homomorphism from ``StandardModel.GaugeGroupℤ₆ to ``GaugeGroupℤ₂
     induced by ``embedSM."
-  deps :≈ [``embedSM, ``StandardModel.GaugeGroupℤ₆, ``GaugeGroupℤ₂,
+  deps :≈ [``inclSM, ``StandardModel.GaugeGroupℤ₆, ``GaugeGroupℤ₂,
     ``sm_ℤ₆_factor_through_gaugeGroupℤ₂SubGroup]
 
 end PatiSalam
