fd1f47e6fe723fc4d5c269185410544f2202d4b7
jstoobysmith <72603918+jstoobysmith@users.noreply.github.com>
2024-12-02 11:15:41 +0000

    chore: Add momentum and position space files.

---
+++ HepLean/FeynmanDiagrams/Wick/MomentumSpace.lean
@@ -0,0 +1,20 @@
+/-
+Copyright (c) 2024 Joseph Tooby-Smith. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Joseph Tooby-Smith
+-/
+import HepLean.FeynmanDiagrams.Basic
+import HepLean.Meta.Informal
+/-!
+
+# Wick contraction in momentum space
+
+Every complete Wick contraction leads to a function on momenta, following
+the Feynman rules.
+
+-/
+
+namespace Wick
+
+
+end Wick
