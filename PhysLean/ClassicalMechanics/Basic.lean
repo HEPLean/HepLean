a6fb39cff9598832dfefc0853e0f33b0d8eaf285
jstoobysmith <72603918+jstoobysmith@users.noreply.github.com>
2025-02-28 15:57:19 +0000

    refactor: Add place holder files

---
+++ PhysLean/ClassicalMechanics/Basic.lean
@@ -0,0 +1,17 @@
+/-
+Copyright (c) 2025 Joseph Tooby-Smith. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Joseph Tooby-Smith
+-/
+/-
+Copyright (c) 2025 Joseph Tooby-Smith. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Joseph Tooby-Smith
+-/
+/-!
+
+# Classical mechanics
+
+This directory is currently a place holder.
+Please feel free to contribute!
+-/
